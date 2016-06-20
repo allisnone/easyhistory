e487be5721a64680fece434fc19384987119bbe9
Administrator <Administrator@USER-20150725QL>
2016-06-20 08:18:19 +0800

    u

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import easyhistory
 #easyhistory.init('D', export='csv', path="C:/hist",stock_codes=['300162','000060'])
-easyhistory.update(path="C:/hist",stock_codes=['000042','000060'])
+#easyhistory.update(path="C:/hist",stock_codes=['000042','000060'])
+easyhistory.update_single_code(dtype='D', stock_code='002521', path="C:/hist")
 
